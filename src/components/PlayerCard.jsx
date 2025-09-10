1eb45869bcf2fa8857c25e1685da42d496f9b616
Taanveer22 <taanveer469@gmail.com>
2025-09-10 16:52:46 +0600

    alhamdulliah icons added to the card

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,24 +1,34 @@
+import { FaUserCircle } from "react-icons/fa";
+import { FaFlag } from "react-icons/fa6";
 const PlayerCard = ({ element }) => {
   const { image, name, country, role, battingHand, bowlingHand, price } =
     element;
   return (
     <div>
-      <div className="card bg-base-100 shadow-sm p-5 space-y-2">
-        <figure>
-          <img className="w-full h-64" src={image} />
-        </figure>
-        <h2 className="card-title">{name}</h2>
-        <div className="flex justify-between border-b-2 border-b-gray-300">
-          <p className="mb-2">{country}</p>
-          <p className="mb-2">{role}</p>
+      <div className="card shadow-sm p-5 space-y-2">
+        <div className="flex items-center">
+          <img className="w-full h-64 rounded-lg" src={image} />
         </div>
-        <h2 className="card-title">Rating</h2>
-        <div className="flex justify-between">
-          <p className="">{battingHand}</p>
-          <p className="">{bowlingHand}</p>
+        <div className="flex items-center gap-3 text-xl font-semibold">
+          <FaUserCircle />
+          <h2>{name}</h2>
         </div>
-        <div className="card-actions justify-between items-center">
-          <button>Price: {price}</button>
+        <div className="flex justify-between text-base font-normal opacity-70">
+          <p className="flex gap-3 items-center">
+            <FaFlag />
+            <span> {country}</span>
+          </p>
+          <button className="btn">{role}</button>
+        </div>
+        <h2 className="card-title pt-2 border-t-2 border-t-gray-300">Rating</h2>
+        <div className="flex justify-between text-base font-normal opacity-70">
+          <p>{battingHand}</p>
+          <p>{bowlingHand}</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <button className="text-base font-normal opacity-70">
+            Price: ${price}
+          </button>
           <button className="btn btn-warning">Choose Player</button>
         </div>
       </div>
