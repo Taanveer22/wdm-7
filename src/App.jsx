65cf750f3c475ebc4ed85d9f5d711b5597db8493
Taanveer22 <taanveer469@gmail.com>
2025-09-14 16:04:53 +0600

    alhamdulliah react toastify added to the project

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import MainContainer from "./components/MainContainer";
 import NewsLetter from "./components/NewsLetter";
 import Footer from "./components/Footer";
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
   const [isActive, setIsActive] = useState({
@@ -41,14 +42,24 @@ function App() {
     // console.log(isExist);
     let hasCoins = coins < element.price;
     // console.log(hasCoins);
-    if (isExist || hasCoins) {
-      alert("not enough coins or already added");
+    let maxPlayers = chosenPlayers.length >= 6;
+    // console.log(maxPlayers);
+    if (isExist || hasCoins || maxPlayers) {
+      if (hasCoins) {
+        toast.error("not enough coins");
+      } else if (isExist) {
+        toast.warning("player already added");
+      } else if (maxPlayers) {
+        toast.info("max player reached");
+      }
     } else {
       const newChosenPlayers = [...chosenPlayers, element];
       setChosenPlayers(newChosenPlayers);
       setCoins(coins - element.price);
+      toast.success("successfully player added");
     }
   };
+
   return (
     <div>
       <div className="w-11/12 mx-auto">
@@ -60,6 +71,7 @@ function App() {
           isActive={isActive}
           handleActiveBtn={handleActiveBtn}
         ></MainContainer>
+        <ToastContainer></ToastContainer>
         <NewsLetter></NewsLetter>
       </div>
       <Footer></Footer>
--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -4,6 +4,7 @@ import { FaFlag } from "react-icons/fa6";
 const PlayerCard = ({ element, handleChoosePlayerBtn }) => {
   const { image, name, country, role, battingHand, bowlingHand, price } =
     element;
+
   return (
     <div>
       <div className="card shadow-sm border-2 border-gray-100 p-5 space-y-2">
